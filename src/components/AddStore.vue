29f8c41b5757b423a38304611893a76607962e28
2-jun9 <jungu269@gmail.com>
2024-09-06 16:13:55 +0900

    Add: 가게 관리 기능

---
+++ src/components/AddStore.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="tab">
+    <Form @submit.prevent="submitForm" class="form">
+      <input
+        v-model="formData.storename"
+        id="storename"
+        type="text"
+        placeholder="storename"
+      />
+      <input
+        v-model="formData.storedetail"
+        id="storedetail"
+        type="text"
+        placeholder="storedetail"
+      />
+      <button type="submit">등록하기</button>
+    </Form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      // form 데이터 저장
+      formData: {
+        storename: "",
+        storedetail: "",
+      },
+    };
+  },
+  methods: {
+    async submitForm() {
+      try {
+        // formData를 Axios를 통해 서버로 전송
+        let ownername = localStorage.getItem("ownername");
+        const response = await axios.post(
+          `http://localhost:8080/members/store/${ownername}/add`,
+          this.formData
+        );
+        alert("가게등록 완료!");
+      } catch (error) {
+        console.error("Error:", error);
+        alert("가게 등록 중 문제가 발생했습니다.");
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/ListView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <h1>운영중인 상점</h1>
+      <button @click="openModal = true">등록하기</button>
+      <div v-if="openModal == true">
+        <AddStore />
+      </div>
+    </div>
+
+    <div>상점 리스트 뿌리기</div>
+    <div>
+      <div>로그아웃</div>
+      <div>동네 소식 나누러가기</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AddStore from "@/components/AddStore.vue";
+
+export default {
+  components: {
+    AddStore,
+  },
+  data() {
+    return {
+      openModal: false,
+    };
+  },
+};
+</script>
